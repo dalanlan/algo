5c5630a713cdc1f57947a9bcb412029eaece42f9
He Simei <hesimei@zju.edu.cn>
2015-09-03 23:14:43 +0800

    drive me crazy

---
+++ majorityElementII.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> majorityElement(vector<int>& nums) {
+    vector<int> res;
+    if(nums.size()==0)
+        return res;
+        
+    int can1=nums[0],can2=0,count1=1,count2=0;
+    
+    for(int i=1;i<nums.size();i++)
+    {
+        
+        if(nums[i]==can1)
+            count1++;
+        else if(nums[i]==can2 && can1 != can2)
+            count2++;
+        else if(count1==0)
+        {
+            can1=nums[i];
+            count1=1;
+        }
+        else if(count2<=0 && can1 != can2)
+        {
+            can2=nums[i];
+            count2=1;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+        
+            
+    }
+    
+        count1=0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i] == can1)
+                count1++;
+        if(count1 > nums.size()/3)
+            res.push_back(can1);
+    
+    
+        count2=0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i] == can2)
+                count2++;
+        if(count2 > nums.size()/3)
+            res.push_back(can2);
+    
+    return res;
+    }
+
+    int main()
+    {   
+        std::vector<int> v(3,0);
+        cout<<majorityElement(v) <<endl;
+        return 0;
+    }
