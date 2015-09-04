fb1e6d878cc05488f316ae792f0d6e6e177d7a73
He Simei <hesimei@zju.edu.cn>
2015-09-04 10:32:39 +0800

    a plain solution for bst

--- kthSmallest.cpp
+++ kthSmallest.cpp
@@ -1,3 +1,5 @@
+// solution 1
+// global var vector<int>
 vector<int> data;
 int kthSmallest(TreeNode* root, int k) 
 {
@@ -17,3 +19,30 @@ int kthSmallest(TreeNode* root, int k)
     return data[k-1];
     
 }
+
+
+// solution 2
+// global var res,order
+int res, order=0;
+
+void dfs(TreeNode* root,int k)
+{
+    if(root->left)
+        dfs(root->left,k);
+    order++;
+    if(order == k)
+    {
+        res = root->val;
+        return;
+    }
+    if(root->right)
+        dfs(root->right,k);
+}
+
+int kthSmallest(TreeNode* root, int k)
+{
+    dfs(root,k);
+    return res;
+}
+
+
