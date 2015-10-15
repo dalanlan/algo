8b07a1bb7f7953e447543841e89d9d463d8f19ad
He Simei <hesimei@zju.edu.cn>
2015-10-15 17:07:46 +0800

    hahha fake

---
+++ orthonormalBasis.cpp
@@ -0,0 +1,15 @@
+//calculate orthonormal basis
+#include<iostream>
+using namespace std;
+
+int main() {
+	double res;
+
+	//double x = -0.297, y=.890, z=-.346;  //D
+	//double x = -.857, y = .286, z = .429;  //A
+	double A = 2/(double)7, B = 3/(double)7, C=6/(double)7;
+
+	res =  A*x + B*y + C*z;
+	cout<<res<<endl;
+	return 0;
+}
