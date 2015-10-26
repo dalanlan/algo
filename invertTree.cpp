f7b3974a41f8868f5aa4be12c5650394d623881f
Emma He <dalanlan925@gmail.com>
2015-10-26 18:34:37 +0800

    "so fuck off"

---
+++ invertTree.cpp
@@ -0,0 +1,11 @@
+    void invert(TreeNode* root) {
+        if(root == NULL)
+            return;
+        invert(root->left);
+        invert(root->right);
+        swap(root->left,root->right);
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        invert(root);
+        return root;
+    }
