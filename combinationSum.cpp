9ab586482103e13cde1c9240ffd861454890f5d0
He Simei <hesimei@zju.edu.cn>
2015-10-30 18:02:40 +0800

    like what, got a cold

---
+++ combinationSum.cpp
@@ -0,0 +1,28 @@
+void sum(vector<int>& candidates, int target, int index, vector<vector<int>>& res, vector<int>& path) {
+    for(int j = index; j < candidates.size(); j++) {
+        if(candidates[j] > target)
+            break;
+        if(candidates[j] == target) {
+            path.push_back(candidates[j]);
+            res.push_back(path);
+            path.pop_back();
+            
+        }
+        else {
+            path.push_back(candidates[j]);
+            sum(candidates,target - candidates[j], j, res, path);
+            path.pop_back();
+        }
+        
+    }
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> res;
+    
+    vector<int> path;
+    sort(candidates.begin(), candidates.end());
+    sum(candidates, target, 0, res, path);
+
+    return res;
+    
+}
