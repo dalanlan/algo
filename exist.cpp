7099300ab79c79528577b497528ed671df3b547a
He Simei <hesimei@zju.edu.cn>
2015-11-16 21:10:48 +0800

    finally kick-it-off

--- exist.cpp
+++ exist.cpp
@@ -1,30 +1,26 @@
-void bfs(vector<vector<char>>& board, vector<vector<int>>& visit, int row, int col, string word, int& index) {
-	cout<<row<<" "<<col<<" ,index: "<<index<<endl;
-	index++;
-	visit[row][col] = 1;
-	if(index == word.length())
-		return;
+    bool dfs(vector<vector<char>>& board, vector<vector<int>>& visit, int row, int col, string word, int index) {
+	int rowMax = board.size();
+	int colMax = board[0].size();
+
+	if(row >= rowMax || row < 0 || col >= colMax || col < 0) return false;
 	
-	if(row > 0 && visit[row-1][col] == 0 && board[row-1][col] == word[index]) { // left
-		cout<<"up"<<endl;
-		bfs(board, visit, row-1, col, word, index);
-		
+	if(board[row][col] == word[index] && visit[row][col] == 0){ //??? 
+		visit[row][col] = 1;
+		if (index == word.length()-1) {
+			return true;
+		}
+		else {
+			if(bfs(board, visit, row+1, col, word, index+1) || bfs(board, visit, row, col+1 , word, index+1) ||
+			bfs(board, visit, row-1, col, word, index+1)||bfs(board, visit, row, col-1, word, index+1)) {
+				return true;
+			}
+
+		}	
+		visit[row][col] = 0;
+
 	}
-	if(col > 0 && visit[row][col-1] == 0 && board[row][col-1] == word[index]) { 
-		cout<<"left"<<endl;
-		bfs(board, visit, row, col-1, word, index);
-		
-	}
-	if(row < board.size() - 1 && visit[row+1][col] == 0 && board[row+1][col] == word[index]) {
-		cout<<"down"<<endl;
-		bfs(board, visit, row+1, col, word, index);
-		
-	}
-	if(col < board[0].size() - 1 && visit[row][col+1] == 0 && board[row][col+1] == word[index]) {
-		cout<<"right"<<endl;
-		bfs(board, visit, row, col+1, word, index);
-		
-	}
+	return false;
+	
 
 	
 }
@@ -33,25 +29,21 @@ bool exist(vector<vector<char>>& board, string word) {
 		return false;
 	int row = board.size();
 	int col = board[0].size();
-	int index = 0;
 	vector<vector<int>> visit(row, vector<int>(col, 0));
+	bool res = false;
+
+	for(int i=0; i< row;i++) {
+		for(int j=0;j < col;j++) {
+			if(dfs(board, visit, i, j, word, 0)){
+				res = true;
+				break;
+			}
+
+		}
+
+	}
 	
 
-	for(int i = 0; i < row; i++) {
-		for(int j = 0; j < col; j++) {
-			if(visit[i][j] == 0 && board[i][j] == word[index]) {
-				bfs(board, visit, i, j, word, index);
-				
-	            if(index == word.length())
-		            return true;
-		        else {
-		            index = 0;
-		            vector<vector<int>> visit(row, vector<int>(col, 0));
-		        }
-			}
-		}
-	}
-
-	return false;
+	return res;
 
 }
