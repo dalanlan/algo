6dcd4700208c0fcaa0c9d716269aa7ba57112695
He Simei <hesimei@zju.edu.cn>
2015-11-21 21:44:57 +0800

    life is short, let's use python

--- fourSum.cpp
+++ fourSum.cpp
@@ -42,3 +42,59 @@ Expected answer:
         
         return res;
     }
+
+
+
+/* Supposed to be the right answer*/
+
+       void twoSum(vector<int>& nums, vector<vector<int>>& res, int target, int index, int prev2, int prev1) {
+    	if(index > nums.size()-2)
+    		return;
+    	int lo=index, hi=nums.size()-1, sum;
+    	vector<int> path;
+
+   		while(lo < hi) {
+   			sum = nums[lo] + nums[hi];
+   			if(sum == target) {
+   				path.push_back(prev1);
+   				path.push_back(prev2);
+   				path.push_back(nums[lo]);
+   				path.push_back(nums[hi]);
+   				res.push_back(path);
+   				path.clear();
+
+   				while(lo+1 < nums.size() && nums[lo] == nums[lo+1]) lo++;
+   				while(hi-1 > -1 && nums[hi] == nums[hi-1]) hi--;
+
+   			}
+   			if(sum < target)
+   				lo++;
+   			else 
+   				hi--;
+   		}
+   		
+
+    }
+
+    void threeSum(vector<int>& nums, vector<vector<int>>& res, int target, int index, int prev) {
+    	if(index > nums.size()-3)
+    		return;
+    	for(int i=index;i<nums.size();i++) {
+    		if(i > index && nums[i] == nums[i-1]) continue;
+    		twoSum(nums, res, target-nums[i], i+1, nums[i], prev);
+    	}
+    }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    	vector<vector<int>> res;
+    	if(nums.size() < 4)
+    		return res;
+        sort(nums.begin(), nums.end());
+    	for(int i=0; i<nums.size();i++) {
+    		if(i > 0 && nums[i] == nums[i-1]) continue;
+    		threeSum(nums, res, target - nums[i], i+1, nums[i]);
+    	}
+    	return res;
+    }
+
+
+    /** Isn't there any convenient way? **/
---
+++ sumRangeQuery.cpp
@@ -0,0 +1,107 @@
+// Range Sum Query
+// The trick is that we need to update the value 
+// every now and then
+// Tags: Segment Tree, Binary Indexed Tree
+
+
+// My solution: Time Exceed 
+    vector<int> sum;
+    vector<int> tmp;
+    NumArray(vector<int> &nums) {
+        if(nums.size() == 0)
+            return;
+        sum.push_back(nums[0]); // sums[0] = nums[0]
+        tmp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++) {
+            sum.push_back(sum[i-1] + nums[i]);
+            tmp.push_back(nums[i]);
+        }
+        
+    }
+
+    void update(int i, int val) {
+        for(int index=i;index<sum.size();index++) {
+            sum[index] += val-tmp[i]; // 
+        }
+        tmp[i] = val;
+    }
+
+    int sumRange(int i, int j) {
+        if(i == 0)
+            return sum[j];
+        return sum[j]-sum[i-1];
+    }
+
+
+// Still TLE solution
+
+struct treeNode {
+    int start;
+    int end;
+    int val;
+    treeNode* left;
+    treeNode* right;
+    treeNode(int s, int e):start(s),end(e){}
+};
+
+treeNode* root;
+
+treeNode* constructTree(vector<int>& nums, int start, int end) {
+    if(nums.size() == 0 || start > end) {
+        return NULL;
+    }
+    treeNode* root = new treeNode(start, end);
+    if(start == end) {
+        root->val = nums[start];
+        return root;
+    }
+    int mid = start + (end-start)/2;
+    root->left = constructTree(nums, start, mid);
+    root->right = constructTree(nums, mid+1, end);
+    root->val = root->left->val + root->right->val;
+    return root;
+}
+
+NumArray(vector<int>& nums) {
+    root = constructTree(nums, 0, nums.size()-1);
+}
+
+void updateUtil(treeNode* root, int i, int val) {
+    if(root->start == root->end) {
+        root->val = val;
+        return;
+    }
+
+    int mid = root->start + (root->end - root->start)/2;
+    if(i < mid) {
+        updateUtil(root->left, i, val);
+    }
+    if(i > mid) {
+        updateUtil(root->right, i, val);
+    }
+    root->val = root->left->val + root->right->val;
+
+}
+
+void update(int i, int val) {
+    updateUtil(root, i, val);    
+}
+
+int sumRangeUtil(treeNode* root, int i, int j) {
+    if(root->start == root->end) {
+        return root->val;
+    }
+    int mid = root->start + (root->end - root->start)/2;
+    if(j <= mid) {
+        return sumRangeUtil(root->left, i, j);
+    }
+    if(i >= mid + 1) {
+        return sumRangeUtil(root->right, i, j);
+    }
+    return sumRangeUtil(root->left, i, mid) + sumRangeUtil(root->right, mid+1, j);
+
+
+}
+int sumRange(int i, int j){
+    return sumRangeUtil(root, i, j);    
+}
